74c6bf17edbf459f8da681127e0b71e2f94118f8
ckejoM <156849470+ckejoM@users.noreply.github.com>
2024-10-12 23:26:28 +0200

    Adding custom EndpointFilter

--- MinimalAPI/Models/Product.cs
+++ MinimalAPI/Models/Product.cs
@@ -1,8 +1,13 @@
-﻿namespace MinimalAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalAPI.Models
 {
     public class Product
     {
+        [Required(ErrorMessage = "Id can't be blank")]
+        [Range (0, 100000, ErrorMessage = "Id should be between 0 and 100.000")]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Product name can't be blank")]
         public string? ProductName { get; set; }
 
         public override string ToString()
--- MinimalAPI/RouteGroups/ProductsMapGroup.cs
+++ MinimalAPI/RouteGroups/ProductsMapGroup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MinimalAPI.EndpointFilters;
 using MinimalAPI.Models;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 
@@ -26,19 +28,46 @@ namespace MinimalAPI.RouteGroups
 
                 if (product is null)
                 {
-                    context.Response.StatusCode = 400;
-                    await context.Response.WriteAsync("Incorrect Product Id");
-                    return;
+                    return Results.ValidationProblem(new Dictionary<string, string[]> {
+                        { "id", new string[] { "Incorrect Product Id" } }
+                    });
                 }
 
-                await context.Response.WriteAsync(JsonSerializer.Serialize(product.ToString()));
+                return Results.Ok(new { message = product.ToString() });
             });
 
             group.MapPost("/", async (HttpContext context, Product product) =>
             {
                 products.Add(product);
-                await context.Response.WriteAsync("Product added");
-            });
+                return Results.Ok(new { message = "Product added" });
+            })
+                .AddEndpointFilter<CustomEndpointFilter>()
+                .AddEndpointFilter(async (context, next) => 
+                {
+                    var product = context.Arguments.OfType<Product>().FirstOrDefault();
+
+                    if (product is not null)
+                    {
+                        return Results.BadRequest("Product details are not found in the request");
+                    }
+
+                    var validationContext = new ValidationContext(product);
+
+                    var errors = new List<ValidationResult>();
+                    bool isValid = Validator.TryValidateObject(product, validationContext, errors, true);
+
+                    if (!isValid)
+                    {
+                        return Results.BadRequest(errors.FirstOrDefault()?.ErrorMessage);
+                    }
+
+                    //Before logic
+
+                    var result = await next(context);
+
+                    //After logic
+                    return result;
+                });
 
             group.MapPut("/{id:int}", async (HttpContext context, int id, [FromBody] Product productDto) =>
             {
@@ -46,14 +75,12 @@ namespace MinimalAPI.RouteGroups
 
                 if(product is null)
                 {
-                    context.Response.StatusCode = 400;
-                    await context.Response.WriteAsync("Incorrect Product Id");
-                    return;
+                    return Results.BadRequest(new { error = "Incorrect Product Id" });
                 }
 
                 product.ProductName = productDto.ProductName;
 
-                await context.Response.WriteAsync("Product updated");
+                return Results.Ok( new { message = "Product updated" });
             });
 
             group.MapDelete("/{id:int}", async (HttpContext context, int id) =>
@@ -62,14 +89,12 @@ namespace MinimalAPI.RouteGroups
 
                 if (product is null)
                 {
-                    context.Response.StatusCode = 400;
-                    await context.Response.WriteAsync("Incorrect Product Id");
-                    return;
+                    return Results.BadRequest(new { error = "Incorrect Product Id" });
                 }
 
                 products.Remove(product);
 
-                await context.Response.WriteAsync("Product deleted");
+                return Results.Ok( new { message = "Product deleted" });
             });
 
             return group;
